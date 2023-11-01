6d9e50f2e0129496ebf0867e8c49ee38a299f233
srivas8583 <srivas8583@gmail.com>
2023-11-01 08:22:21 +0530

    cloudvandana Assignment

---
+++ src/Task1/ShuffleArray.java
@@ -0,0 +1,18 @@
+package Task1;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class ShuffleArray {
+	public static void main(String[] args) {
+		Integer[] mainArray = {1, 2, 3, 4, 5, 6, 7};
+
+        List<Integer> list = new ArrayList<>(Arrays.asList(mainArray));
+        Collections.shuffle(list);
+        Integer[] shuffledArray = list.toArray(new Integer[0]);
+        System.out.println("Shuffled Array: " + Arrays.toString(shuffledArray));
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author rudeo
+ *
+ */
+module CloudvandanaassigmentASE {
+}
